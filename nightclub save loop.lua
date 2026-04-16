4e02a987960b7a7b4485926da7186b1588959d24
Dinguscool4 <dinguscool4@gmail.com>
2026-04-16 19:39:57 -0400

    Update nightclub save loop.lua

--- nightclub save loop.lua
+++ nightclub save loop.lua
@@ -2,10 +2,20 @@ script.run_in_callback(function()
 	local running = true
 
 	while running == true do
-		stats.set_int("mpx_club_popularity", 1000)
-		stats.set_int("mpx_club_pay_time_left", -1)
+		local isonline =
+			network.network_is_session_started() and
+			not network.network_is_in_transition() and
+			not streaming.is_player_switch_in_progress()
 
-		notify.success("max nightclub safe", "done")
-		script.yield(10000)
+		if isonline then
+			stats.set_int("mpx_club_popularity", 1000)
+			stats.set_int("mpx_club_pay_time_left", -1)
+
+			notify.success("max nightclub safe", "done")
+		else
+			notify.info("max nightclub safe", "lobby not supported")
+		end
+
+		script.yield(5000)
 	end
 end)
